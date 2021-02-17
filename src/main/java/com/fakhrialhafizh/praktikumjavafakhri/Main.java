b818c9950f0ffce745611aaae980aa07f7719674
Fakhri A Al Hafizh <79123080+FakhriAbdurrahmanAlHafizh@users.noreply.github.com>
2021-02-18 02:25:16 +0800

    Uji Koneksi Ke Database

--- src/main/java/com/fakhrialhafizh/praktikumjavafakhri/Main.java
+++ src/main/java/com/fakhrialhafizh/praktikumjavafakhri/Main.java
@@ -1,6 +1,14 @@
 package com.fakhrialhafizh.praktikumjavafakhri;
+
+import com.fakhrialhafizh.praktikumjavafakhri.db.Database;
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        
+        Database db = new Database();
+        Connection con = db.getConnection();
+        
     }
 }
