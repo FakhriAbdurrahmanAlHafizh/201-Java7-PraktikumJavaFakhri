d7f0460bce2e47433d156287c0be838c58531e86
Fakhri A Al Hafizh <79123080+FakhriAbdurrahmanAlHafizh@users.noreply.github.com>
2021-02-18 13:15:28 +0800

    testing read data

--- src/main/java/com/fakhrialhafizh/praktikumjavafakhri/Main.java
+++ src/main/java/com/fakhrialhafizh/praktikumjavafakhri/Main.java
@@ -3,6 +3,7 @@ package com.fakhrialhafizh.praktikumjavafakhri;
 import com.fakhrialhafizh.praktikumjavafakhri.db.Database;
 import com.fakhrialhafizh.praktikumjavafakhri.model.JenisBarang;
 import java.sql.Connection;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,8 +13,12 @@ public class Main {
         Connection con = db.getConnection();
         
         JenisBarang jenisBarang = new JenisBarang(con);
-        jenisBarang.setNamaJenisBarang("TESTING namajenisbarang");
-        jenisBarang.create();
+   //     jenisBarang.setNamaJenisBarang("TESTING namajenisbarang");
+    //    jenisBarang.create();
+        
+        ArrayList<Object> list =  jenisBarang.read();
+        JenisBarang jb = (JenisBarang) list.get(1);
+        System.out.println(jb.getNamaJenisBarang());
         
     }
 }
