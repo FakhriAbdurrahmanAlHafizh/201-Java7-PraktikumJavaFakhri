523c40d9a393d03fb9b5c923b8df354361760b88
Fakhri A Al Hafizh <79123080+FakhriAbdurrahmanAlHafizh@users.noreply.github.com>
2021-02-18 12:00:40 +0800

    mengisi atribut

---
+++ src/main/java/com/fakhrialhafizh/praktikumjavafakhri/model/JenisBarang.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fakhrialhafizh.praktikumjavafakhri.model;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author WIN8
+ */
+public class JenisBarang {
+    Connection con;
+    
+    private int id;
+    private String namaJenisBarang;
+}
