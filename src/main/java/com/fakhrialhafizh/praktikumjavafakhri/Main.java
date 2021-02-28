926218e93370ce88eb0bde1bfa7de7d2f04187b6
Fakhri A Al Hafizh <79123080+FakhriAbdurrahmanAlHafizh@users.noreply.github.com>
2021-02-28 12:13:22 +0800

    mengatur, memanggil, menutup frame

--- src/main/java/com/fakhrialhafizh/praktikumjavafakhri/Main.java
+++ src/main/java/com/fakhrialhafizh/praktikumjavafakhri/Main.java
@@ -1,29 +1,14 @@
 package com.fakhrialhafizh.praktikumjavafakhri;
-
-import com.fakhrialhafizh.praktikumjavafakhri.db.Database;
-import com.fakhrialhafizh.praktikumjavafakhri.model.JenisBarang;
-import java.sql.Connection;
-import java.util.ArrayList;
+        
+import view.cashier.jenisbarang.JenisBarangViewFrame;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        JenisBarangViewFrame frame = new JenisBarangViewFrame();
         
-        Database db = new Database();
-        Connection con = db.getConnection();
-        
-        JenisBarang jenisBarang = new JenisBarang(con);
-   //     jenisBarang.setNamaJenisBarang("TESTING namajenisbarang");
-    //    jenisBarang.create();
-        
-//        ArrayList<Object> list =  jenisBarang.read();
-//        JenisBarang jb = (JenisBarang) list.get(1);
-//        System.out.println(jb.getNamaJenisBarang());
-        
-          jenisBarang.setId(10);
-//        jenisBarang.setNamaJenisBarang("test test");
-//        jenisBarang.update();
-
-            jenisBarang.delete();
+        frame.dispose();
+        frame.setUndecorated(true);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
