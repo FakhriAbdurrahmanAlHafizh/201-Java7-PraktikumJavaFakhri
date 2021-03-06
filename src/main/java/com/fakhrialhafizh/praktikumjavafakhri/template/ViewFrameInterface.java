157c9b1effecebe0013c02d05e6525d7b6289b68
Fakhri A Al Hafizh <79123080+FakhriAbdurrahmanAlHafizh@users.noreply.github.com>
2021-03-06 17:12:28 +0800

    Create ViewFrameInterface.java

---
+++ src/main/java/com/fakhrialhafizh/praktikumjavafakhri/template/ViewFrameInterface.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fakhrialhafizh.praktikumjavafakhri.template;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author WIN8
+ */
+public interface ViewFrameInterface {
+    abstract void refresh();
+    abstract void buildTable(ArrayList<Object> list);
+}
