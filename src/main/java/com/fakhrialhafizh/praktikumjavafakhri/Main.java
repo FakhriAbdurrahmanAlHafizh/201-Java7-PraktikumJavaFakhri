f35a3b1a1c257fa28d6cf06fae88801901e2eb5a
Fakhri A Al Hafizh <79123080+FakhriAbdurrahmanAlHafizh@users.noreply.github.com>
2021-03-07 15:19:16 +0800

    update main

--- src/main/java/com/fakhrialhafizh/praktikumjavafakhri/Main.java
+++ src/main/java/com/fakhrialhafizh/praktikumjavafakhri/Main.java
@@ -1,5 +1,6 @@
 package com.fakhrialhafizh.praktikumjavafakhri;
         
+import com.fakhrialhafizh.praktikumjavafakhri.view.auth.LoginFrame;
 import view.cashier.jenisbarang.JenisBarangViewFrame;
 
 
@@ -7,8 +8,7 @@ import view.cashier.jenisbarang.JenisBarangViewFrame;
 public class Main {
     public static void main(String[] args) {
 
-        JenisBarangViewFrame frame = new JenisBarangViewFrame();
-        frame.customShow();
-        
+        LoginFrame frame = new LoginFrame();
+    frame.customShow();
     }
 }
